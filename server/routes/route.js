3c5808db67e89594570b89eb86e6ed65cbd77a6c
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-04-29 21:53:03 -0500

    Add CORS configuration and route separation for better security and organization

---
+++ server/routes/route.js
@@ -0,0 +1,134 @@
+import { Book } from '../models/bookModel.js'
+import express from 'express'
+
+const router = express.Router();
+
+
+router.get("/", async (req, res) => {
+	try {
+	  const books = await Book.find({});
+	//   res.status(200).send(books); -- my version
+	return res.status(200).json({
+		count: books.length,
+		data: books
+	}) //-- tutorial version
+	} catch (err) {
+	  console.error(err);
+	  res.send("Not found").status(500);
+	}
+      });
+
+
+router.get("/:id", async (req, res) => {
+	try {
+	    const bookId = req.params.id;
+	    const book = await Book.findById(bookId);
+	    if (!book) {
+		return res.status(404).json({ message: "Book not found" });
+	    }
+	    return res.status(200).json(book);
+	} catch (err) {
+	    console.error(err);
+	    return res.status(500).json({ message: "Internal Server Error" });
+	}
+    });
+
+    
+
+router.post("/", async(req,res)=>{
+
+	try{
+		if(
+		!req.body.title||
+		!req.body.page||
+		!req.body.author||
+		!req.body.pubYear
+		){
+			return res.status(400).send({message: `Fill out all required fields: title,page,author,pubYear`})
+		}
+		const newBook = {
+			title: req.body.title,
+			page: req.body.page,
+			author: req.body.author,
+			pubYear: req.body.pubYear
+		}
+
+		const book = await Book.create(newBook);
+
+		return res.status(201).send(book)
+	}catch(err){
+		console.error(err.message)
+		res.send({ message: err.message}).status(500)
+	}
+})
+
+// router.put('/books/:id' , async (req,res)=>{
+// 	try{
+// 		if(
+// 			!req.body.title||
+// 			!req.body.page||
+// 			!req.body.author||
+// 			!req.body.pubYear
+// 			){
+// 				return res.status(400).send({message: `Fill out all required fields: title,page,author,pubYear`})
+// 			}
+// 			const update = {
+// 				title: req.body.title,
+// 				page: req.body.page,
+// 				author: req.body.author,
+// 				pubYear: req.body.pubYear
+// 			}
+// 			const bookId = req.params.id;
+// 			const result = await Book.findByIdAndUpdate(bookId,update);
+
+// 			if(!result){
+// 				return res.status(404).json({message: 'Book not found'})
+// 			}
+// 			return res.status(200).send({message:"Updated successfully"});
+
+// 				}catch(err){
+// 					console.error(err)
+// 				}
+
+// })
+
+router.put('/:id' , async (req,res)=>{
+	try{
+		if(
+			!req.body.title||
+			!req.body.page||
+			!req.body.author||
+			!req.body.pubYear
+			){
+				return res.status(400).send({message: `Fill out all required fields: title,page,author,pubYear`})
+			}
+
+			const {id} = req.params;
+			const result = await Book.findByIdAndUpdate(id,req.body);
+
+			if(!result){
+				return res.status(404).json({message: 'Book not found'})
+			}
+			return res.status(200).send({message:"Updated successfully"});
+
+				}catch(err){
+					console.error(err)
+				}
+
+})
+
+
+router.delete('/:id', async (req,res)=>{
+	try{
+		const {id} = req.params;
+		const result = await Book.findByIdAndDelete(id)
+
+		res.status(200).send('Book successfully deleted')
+
+	}catch(err){
+		console.error(err)
+	}
+})
+
+
+export default router;
--- server/server.js
+++ server/server.js
@@ -1,66 +1,28 @@
-import express from 'express'
-import cors from 'cors'
-// import books from './routes/route.js'
-import connectDB from './db/connection.js'
-import { Book } from './models/bookModel.js'
-
-
+import express from "express";
+import cors from "cors";
+import books from "./routes/route.js";
+import connectDB from "./db/connection.js";
 
 const server = express();
-const PORT = process.env.PORT || 5050
+const PORT = process.env.PORT || 5050;
 
-server.use(cors())
-server.use(express.json())
-// server.use("/books", books)
+server.use(cors()); // by default allows all origins with default of cors(*)
+server.use(
+  cors({
+    origin: "http://localhost:5050",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-type"],
+  })
+);
 
-server.get('/', (req,res)=>{
-	res.send(`Welcome to your server at port: ${PORT}`).status(200)
-})
+server.use(express.json());
+server.use("/books", books);
 
+server.get("/", (req, res) => {
+  res.send(`Welcome to your server at port: ${PORT}`).status(200);
+});
 
-server.get("/books", async (req, res) => {
-	try {
-	  const books = await Book.find({});
-	  res.status(200).send(books);
-	} catch (err) {
-	  console.error(err);
-	  res.send("Not found").status(500);
-	}
-      });
-
-
-
-server.post("/books", async(req,res)=>{
-
-	try{
-		if(
-		!req.body.title||
-		!req.body.page||
-		!req.body.author||
-		!req.body.pubYear
-		){
-			return res.status(400).send({message: `Fill out all required fields: title,page,author,pubYear`})
-		}
-		const newBook = {
-			title: req.body.title,
-			page: req.body.page,
-			author: req.body.author,
-			pubYear: req.body.pubYear
-		}
-
-		const book = await Book.create(newBook);
-
-		return res.status(201).send(book)
-	}catch(err){
-		console.error(err.message)
-		res.send({ message: err.message}).status(500)
-	}
-})
-
-
-      
-
-server.listen(PORT,async()=>{
-	await connectDB();
-	console.log(`The server is running on the following port: ${PORT}`)
-})
+server.listen(PORT, async () => {
+  await connectDB();
+  console.log(`The server is running on the following port: ${PORT}`);
+});
