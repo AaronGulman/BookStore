525e08bca379542eafaddb1e001e0ae03e7471de
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-05-18 21:35:49 -0400

    Apply all the necessary changes to make the component work properly

--- client/src/pages/EditBook.jsx
+++ client/src/pages/EditBook.jsx
@@ -1,18 +1,36 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import Spinner from "../components/Spinner.jsx";
 import BackButton from "../components/BackButton.jsx";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
-const CreateBook = () => {
+const EditBook = () => {
   const [title, setTitle] = useState("");
   const [author, setAuthor] = useState("");
   const [page, setPage] = useState("");
   const [pubYear, setPubYear] = useState("");
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
+  const { id } = useParams();
 
-  const handleSaveBook = () => {
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get(`http://localhost:5050/books/${id}`)
+      .then((res) => {
+        setAuthor(res.data.author);
+        setPubYear(res.data.pubYear);
+	setPage(res.data.page);
+        setTitle(res.data.title);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setLoading(false);
+        console.error(err);
+      });
+  }, []);
+
+  const handleEditBook = () => {
     // Validate the data before sending
     if (!title || !author || !page || !pubYear) {
       alert("All fields are required");
@@ -37,7 +55,7 @@ const CreateBook = () => {
 
     setLoading(true);
     axios
-      .post(`http://localhost:5050/books`, data)
+      .put(`http://localhost:5050/books/${id}`, data)
       .then(() => {
         setLoading(false);
         navigate("/");
@@ -52,7 +70,7 @@ const CreateBook = () => {
   return (
     <div className="p-4">
       <BackButton />
-      <h1 className="text-3xl my-4">Create a new book</h1>
+      <h1 className="text-3xl my-4">Edit</h1>
       {loading ? <Spinner /> : ""}
       <div className="flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto">
         <div className="my-4">
@@ -91,7 +109,7 @@ const CreateBook = () => {
             className="border-2 border-gray-500 px-4 py-2 w-full"
           />
         </div>
-        <button className="p-2 bg-sky-300 m-8" onClick={handleSaveBook}>
+        <button className="p-2 bg-sky-300 m-8" onClick={handleEditBook}>
           Save
         </button>
       </div>
@@ -99,4 +117,4 @@ const CreateBook = () => {
   );
 };
 
-export default CreateBook;
+export default EditBook;
