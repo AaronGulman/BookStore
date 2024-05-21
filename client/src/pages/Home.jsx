99cc8f85ff5796a6fc1a2312e78535353bdc9540
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-05-20 23:54:44 -0400

    Format the table element within Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -40,20 +40,36 @@ const Home = () => {
           <table className="w-full border-collapse overflow-hidden rounded-lg shadow-lg">
             <thead>
               <tr className="bg-gray-200">
-                <th className="px-4 py-2 border border-gray-300 rounded-tl-lg">No</th>
+                <th className="px-4 py-2 border border-gray-300 rounded-tl-lg">
+                  No
+                </th>
                 <th className="px-4 py-2 border border-gray-300">Title</th>
-                <th className="px-4 py-2 border border-gray-300 max-md:hidden">Author</th>
-                <th className="px-4 py-2 border border-gray-300 max-md:hidden">Year</th>
-                <th className="px-4 py-2 border border-gray-300 rounded-tr-lg">Operations</th>
+                <th className="px-4 py-2 border border-gray-300 max-md:hidden">
+                  Author
+                </th>
+                <th className="px-4 py-2 border border-gray-300 max-md:hidden">
+                  Year
+                </th>
+                <th className="px-4 py-2 border border-gray-300 rounded-tr-lg">
+                  Operations
+                </th>
               </tr>
             </thead>
             <tbody>
               {books.map((book, index) => (
                 <tr key={book._id} className="bg-white even:bg-gray-100">
-                  <td className="px-4 py-2 border border-gray-300 text-center">{index + 1}</td>
-                  <td className="px-4 py-2 border border-gray-300 text-center">{book.title}</td>
-                  <td className="px-4 py-2 border border-gray-300 text-center max-md:hidden">{book.author}</td>
-                  <td className="px-4 py-2 border border-gray-300 text-center max-md:hidden">{book.pubYear}</td>
+                  <td className="px-4 py-2 border border-gray-300 text-center">
+                    {index + 1}
+                  </td>
+                  <td className="px-4 py-2 border border-gray-300 text-center">
+                    {book.title}
+                  </td>
+                  <td className="px-4 py-2 border border-gray-300 text-center max-md:hidden">
+                    {book.author}
+                  </td>
+                  <td className="px-4 py-2 border border-gray-300 text-center max-md:hidden">
+                    {book.pubYear}
+                  </td>
                   <td className="px-4 py-2 border border-gray-300 text-center">
                     <div className="flex justify-center gap-x-4">
                       <Link to={`/books/details/${book._id}`}>
