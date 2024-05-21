f65cd450afa40f1eb7aadcdf44e166c156609e98
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-05-21 02:06:33 -0400

    Import DeleteBook.jsx to App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import CreateBook from "./pages/CreateBook";
-// import DeleteBook from "./pages/DeleteBook";
+import DeleteBook from "./pages/DeleteBook";
 import EditBook from "./pages/EditBook";
 import ShowBook from "./pages/ShowBook";
 
@@ -12,7 +12,7 @@ const App = () => {
       <Route path="/books/create" element={<CreateBook />} />
       <Route path="/books/details/:id" element={<ShowBook />} />
       <Route path="/books/edit/:id" element={<EditBook />} />
-      {/* <Route path="/books/delete/:id" element={<DeleteBook />}/> */}
+      <Route path="/books/delete/:id" element={<DeleteBook />}/>
     </Routes>
   );
 };
