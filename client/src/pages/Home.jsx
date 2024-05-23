1574f01f9558cde0d7ba16ccb17a69b9273df63b
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-05-22 23:13:56 -0400

    Import BooksTable and add replace the table element with the BooksTable component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -6,10 +6,13 @@ import { Link } from "react-router-dom";
 import { AiOutlineEdit } from "react-icons/ai";
 import { BsInfoCircle } from "react-icons/bs";
 import { MdOutlineAddBox, MdOutlineDelete } from "react-icons/md";
+import BooksTable from "../components/home/BooksTable.jsx";
+import BooksCard from "../components/home/BooksCard.jsx";
 
 const Home = () => {
   const [books, setBooks] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [showType, setShowType] = useState('table');
 
   useEffect(() => {
     setLoading(true);
@@ -33,62 +36,8 @@ const Home = () => {
           <MdOutlineAddBox className="text-sky-800 text-4xl" />
         </Link>
       </div>
-      {loading ? (
-        <Spinner />
-      ) : (
-        <div className="overflow-x-auto">
-          <table className="w-full border-collapse overflow-hidden rounded-lg shadow-lg">
-            <thead>
-              <tr className="bg-gray-200">
-                <th className="px-4 py-2 border border-gray-300 rounded-tl-lg">
-                  No
-                </th>
-                <th className="px-4 py-2 border border-gray-300">Title</th>
-                <th className="px-4 py-2 border border-gray-300 max-md:hidden">
-                  Author
-                </th>
-                <th className="px-4 py-2 border border-gray-300 max-md:hidden">
-                  Year
-                </th>
-                <th className="px-4 py-2 border border-gray-300 rounded-tr-lg">
-                  Operations
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {books.map((book, index) => (
-                <tr key={book._id} className="bg-white even:bg-gray-100">
-                  <td className="px-4 py-2 border border-gray-300 text-center">
-                    {index + 1}
-                  </td>
-                  <td className="px-4 py-2 border border-gray-300 text-center">
-                    {book.title}
-                  </td>
-                  <td className="px-4 py-2 border border-gray-300 text-center max-md:hidden">
-                    {book.author}
-                  </td>
-                  <td className="px-4 py-2 border border-gray-300 text-center max-md:hidden">
-                    {book.pubYear}
-                  </td>
-                  <td className="px-4 py-2 border border-gray-300 text-center">
-                    <div className="flex justify-center gap-x-4">
-                      <Link to={`/books/details/${book._id}`}>
-                        <BsInfoCircle className="text-2xl text-green-800" />
-                      </Link>
-                      <Link to={`/books/edit/${book._id}`}>
-                        <AiOutlineEdit className="text-2xl text-yellow-600" />
-                      </Link>
-                      <Link to={`/books/delete/${book._id}`}>
-                        <MdOutlineDelete className="text-2xl text-red-600" />
-                      </Link>
-                    </div>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      )}
+      {loading ?
+        <Spinner /> : <BooksTable books={books} /> }
     </div>
   );
 };
