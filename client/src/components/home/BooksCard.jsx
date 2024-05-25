9d049e5f66e6f1324ddbd96fabc20e0ce0ad8eec
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-05-24 23:56:42 -0400

    Make BooksCard component responsive with grid layout adjustments

--- client/src/components/home/BooksCard.jsx
+++ client/src/components/home/BooksCard.jsx
@@ -8,7 +8,7 @@ import { BsInfoCircle } from "react-icons/bs";
 
 const BooksCard = () => {
   return (
-    <div>
+    <div className='grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
       
     </div>
   )
