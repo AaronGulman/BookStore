ca5d6b36faf64857ef19c674950ee35466daedf3
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-05-29 23:33:47 -0400

    feat: Enhance BookModal with book details and improved styling
    - Added React import
    
    - Included publication year, title, and author fields in the modal
    
    - Added additional styling for book details
    
    - Fixed typo in class name for AiOutlineClose icon
    
    - Adjusted onClick to properly stop propagation
    
    - Included placeholder text for additional content

--- client/src/components/home/BookModal.jsx
+++ client/src/components/home/BookModal.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { AiOutlineClose } from "react-icons/ai";
 import { BiUserCircle } from "react-icons/bi";
 import { PiBookOpenTextLight } from "react-icons/pi";
@@ -9,13 +10,33 @@ const BookModal = ({ book, onClose }) => {
       onClick={onClose}
     >
       <div
-        onClick={(event) => event.stopPropagation}
+        onClick={(event) => event.stopPropagation()}
         className="w-[600px] max-w-full h-[400px] bg-white rounded-xl p-4 flex flex-col relative"
       >
         <AiOutlineClose
-          className="absolute right-6 text-3x1 text-red-600 cursor-pointer"
+          className="absolute right-6 text-3xl text-red-600 cursor-pointer"
           onClick={onClose}
         />
+        <h2 className="w-fit px-4 py-1 bg-red-300 rounded-lg">
+          {book.pubYear}
+        </h2>
+        <div className="my-2 text-gray-500">
+          <div className="flex items-center gap-x-2">
+            <PiBookOpenTextLight className="text-red-300 text-2xl" />
+            <h2 className="my-1">{book.title}</h2>
+          </div>
+          <div className="flex items-center gap-x-2 mt-2">
+            <BiUserCircle className="text-red-300 text-2xl" />
+            <h2 className="my-1">{book.author}</h2>
+          </div>
+          <p className="mt-4">Show here</p>
+          <p className="my-2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore,
+            similique ipsum perspiciatis ea officia consectetur sequi
+            blanditiis aperiam tenetur, voluptatibus sed aliquam sit
+            reprehenderit quia tempore molestiae molestias, itaque a!
+          </p>
+        </div>
       </div>
     </div>
   );
